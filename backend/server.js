39bad6e98a0cee90444d541efb23ea09f97bbb0c
pritha-techformation <pritha@techformation.io>
2026-03-11 05:43:19 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,5 @@ const connectDB = require("./src/config/db");
 // Connect to MongoDB
 connectDB();
 
-// Start the server
-app.listen(process.env.PORT, () =>
-  console.log(`Server running on port ${process.env.PORT}`)
-);
+// export the app
+module.exports = app;
